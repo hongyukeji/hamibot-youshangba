ab02224d49ed6316e525736fca1e735c7f3ba891
hongyu <1527200768@qq.com>
2022-11-14 15:04:11 +0800

    Initial commit

--- src/index.ts
+++ src/index.ts
@@ -24,21 +24,25 @@ var waitTimeMillisecond = intervalTimeMillisecond * 2;
 app.launchApp('优赏吧');
 // app.launch('com.hamibot.hamibot');
 
-toastLog("优赏吧启动成功！");
+toastLog("请在运行前，将优赏吧APP关闭，或保持在首页页面！");
 
 sleep(waitTimeMillisecond);
 
-var skipBtnElement = className("android.widget.ImageView").descContains("跳过");
-if (skipBtnElement.exists()) {
-    // cn.originalstar.fluocean.SplashActivity
-    skipBtnElement.findOne().click();
-    toastLog("广告跳过成功！");
-}
+try {
+    var skipBtnElement = className("android.widget.ImageView").descContains("跳过");
+    if (skipBtnElement.exists()) {
+        // cn.originalstar.fluocean.SplashActivity
+        skipBtnElement.findOne().click();
+        toastLog("广告跳过成功！");
+    }
 
-var skipBtn = id("tt_splash_skip_btn");
-if (skipBtn.exists()) {
-    skipBtn.findOne().click();
-    toastLog("广告跳过成功！");
+    var skipBtn = id("tt_splash_skip_btn");
+    if (skipBtn.exists()) {
+        skipBtn.findOne().click();
+        toastLog("广告跳过成功！");
+    }
+} catch (e) {
+    console.log("广告跳过异常", e);
 }
 
 // app.startActivity('console'); // 打开控制台(日志)
@@ -46,128 +50,133 @@ if (skipBtn.exists()) {
 
 var myBtn = className("android.widget.ImageView").desc("我的");
 myBtn.waitFor();
-if (myBtn.exists()) {
+toastLog("优赏吧启动成功！");
+/*if (myBtn.exists()) {
     // var currentActivity = currentActivity();
     // com.example.zhuoxin_task.MainActivity
     // alert("当前活动组件 " + currentActivity);
+} else {
+    toastLog("请关闭当前App，然后再启动脚本！");
+}*/
 
-    myBtn.findOne().click();
-    toastLog("进入[我的]页面成功！");
+myBtn.findOne().click();
+toastLog("进入[我的]页面成功！");
+
+sleep(waitTimeMillisecond);
+
+while (true) {
+    var myShopBtn = className("android.widget.ImageView").desc("我的店铺");
+    myShopBtn.waitFor();
+    // if (myShopBtn.exists()) {}
+
+    myShopBtn.findOne().click();
+    toastLog("进入[我的店铺]页面成功！");
 
     sleep(waitTimeMillisecond);
 
-    while (true) {
-        var myShopBtn = className("android.widget.ImageView").desc("我的店铺");
-        myShopBtn.waitFor();
-        if (myShopBtn.exists()) {
-            myShopBtn.findOne().click();
-            toastLog("进入[我的店铺]页面成功！");
+    var taskRunListBtn = className("android.view.View").descContains("进行中");
+    taskRunListBtn.waitFor();
+    // if (taskRunListBtn.exists()) {}
 
-            sleep(waitTimeMillisecond);
+    taskRunListBtn.findOne().click();
+    toastLog("点击[进行中]按钮成功！");
 
-            var taskRunListBtn = className("android.view.View").descContains("进行中");
-            if (taskRunListBtn.exists()) {
-                taskRunListBtn.findOne().click();
-                toastLog("点击[进行中]按钮成功！");
+    sleep(waitTimeMillisecond);
 
-                sleep(waitTimeMillisecond);
+    // 1.运行任务
+    let taskListBtn = className("android.widget.ImageView").descContains("审核").untilFind().filter(function (w) {
+        // console.log('w', w);
+        if (w.contentDescription != null) {
+            let text = String(w.contentDescription);
+            console.log('text', text);
+            let num = text.split(":")[1];
+            console.log('num', num);
+            if (num && Number(num) > 0) {
+                return true;
+            }
+        }
+        return false;
+    });
 
-                // 1.运行任务
-                let taskListBtn = className("android.widget.ImageView").descContains("审核").untilFind().filter(function (w) {
-                    // console.log('w', w);
-                    if (w.contentDescription != null) {
-                        let text = String(w.contentDescription);
-                        console.log('text', text);
-                        let num = text.split(":")[1];
-                        console.log('num', num);
-                        if (num && Number(num) > 0) {
-                            return true;
+    if (taskListBtn.length) {
+        toastLog("发现任务");
+
+        // 2.处理任务集合
+        for (let taskListBtnElement of taskListBtn) {
+            toastLog("进入任务...");
+            let desc = taskListBtnElement.contentDescription;
+            console.log('desc', desc);
+
+            // 3.进入审核任务(进行中记录)
+            taskListBtnElement.click();
+
+            sleep(intervalTimeMillisecond);
+
+            // 4.获取所有待审核元素
+            var waitTaskList = className("android.view.View").descContains("提交时间").untilFind().filter(function () {
+                return true;
+            });
+            sleep(intervalTimeMillisecond);
+            if (waitTaskList.length) {
+                for (let waitTaskElement of waitTaskList) {
+                    sleep(intervalTimeMillisecond);
+                    // console.log('waitTaskElement', waitTaskElement);
+                    waitTaskElement.click();
+                    sleep(intervalTimeMillisecond);
+                    let passAuditBtn = className("android.view.View").descContains("审核通过");
+                    passAuditBtn.waitFor();
+                    if (passAuditBtn.exists()) {
+                        passAuditBtn.findOne().click();
+
+                        sleep(intervalTimeMillisecond);
+
+                        // 确认通过
+                        let confirmPassAuditBtn = className("android.view.View").descContains("确认通过");
+                        confirmPassAuditBtn.waitFor();
+                        if (confirmPassAuditBtn.exists()) {
+                            console.log('confirmPassAuditBtn', confirmPassAuditBtn.findOne().contentDescription);
+                            // todo: 正式环境打开确认通过
+                            confirmPassAuditBtn.findOne().click();
                         }
+
+                        // todo: 正式环境 注释此处代码
+                        /*let cancelPassAuditBtn = className("android.view.View").descContains("取消");
+                        if (cancelPassAuditBtn.exists()) {
+                            console.log('cancelPassAuditBtn', cancelPassAuditBtn.findOne().contentDescription);
+                            cancelPassAuditBtn.findOne().click();
+                        }*/
+
+                        console.log('开始时间', className("android.view.View").descContains("开始时间").findOne().contentDescription);
+
                     }
-                    return false;
-                });
-
-                if (taskListBtn.length) {
-                    toastLog("发现任务");
-
-                    // 2.处理任务集合
-                    for (let taskListBtnElement of taskListBtn) {
-                        toastLog("进入任务...");
-                        let desc = taskListBtnElement.contentDescription;
-                        console.log('desc', desc);
-
-                        // 3.进入审核任务(进行中记录)
-                        taskListBtnElement.click();
-
-                        sleep(intervalTimeMillisecond);
-
-                        // 4.获取所有待审核元素
-                        var waitTaskList = className("android.view.View").descContains("提交时间").untilFind().filter(function () {
-                            return true;
-                        });
-                        sleep(intervalTimeMillisecond);
-                        if (waitTaskList.length) {
-                            for (let waitTaskElement of waitTaskList) {
-                                sleep(intervalTimeMillisecond);
-                                // console.log('waitTaskElement', waitTaskElement);
-                                waitTaskElement.click();
-                                sleep(intervalTimeMillisecond);
-                                let passAuditBtn = className("android.view.View").descContains("审核通过");
-                                passAuditBtn.waitFor();
-                                if (passAuditBtn.exists()) {
-                                    passAuditBtn.findOne().click();
-
-                                    sleep(intervalTimeMillisecond);
-
-                                    // 确认通过
-                                    let confirmPassAuditBtn = className("android.view.View").descContains("确认通过");
-                                    confirmPassAuditBtn.waitFor();
-                                    if (confirmPassAuditBtn.exists()) {
-                                        console.log('confirmPassAuditBtn', confirmPassAuditBtn.findOne().contentDescription);
-                                        // todo: 正式环境打开确认通过
-                                        confirmPassAuditBtn.findOne().click();
-                                    }
-
-                                    // todo: 正式环境 注释此处代码
-                                    /*let cancelPassAuditBtn = className("android.view.View").descContains("取消");
-                                    if (cancelPassAuditBtn.exists()) {
-                                        console.log('cancelPassAuditBtn', cancelPassAuditBtn.findOne().contentDescription);
-                                        cancelPassAuditBtn.findOne().click();
-                                    }*/
-
-                                    console.log('开始时间', className("android.view.View").descContains("开始时间").findOne().contentDescription);
-
-                                }
-                                sleep(intervalTimeMillisecond);
-                                // 返回按钮
-                                className("android.widget.ImageView").clickable(true).depth(8).findOne().click();
-                                sleep(intervalTimeMillisecond);
-                            }
-                        }
-                        sleep(intervalTimeMillisecond);
-                        className("android.widget.ImageView").clickable(true).depth(8).findOne().click(); // 返回按钮
-                        sleep(intervalTimeMillisecond);
-                        toastLog("已完成当前审核任务");
-                    }
-
-                    if (className("android.view.View").descContains("我的店铺").exists()) {
-                        sleep(intervalTimeMillisecond);
-                        className("android.widget.ImageView").clickable(true).depth(9).findOne().click(); // 返回按钮
-                        sleep(intervalTimeMillisecond);
-                    }
-                } else {
-                    toastLog("未发现任务");
+                    sleep(intervalTimeMillisecond);
+                    // 返回按钮
+                    className("android.widget.ImageView").clickable(true).depth(8).findOne().click();
+                    sleep(intervalTimeMillisecond);
                 }
             }
-
+            sleep(intervalTimeMillisecond);
+            className("android.widget.ImageView").clickable(true).depth(8).findOne().click(); // 返回按钮
+            sleep(intervalTimeMillisecond);
+            toastLog("已完成当前审核任务");
         }
-        sleep(waitTimeMillisecond);
+
+        if (className("android.view.View").descContains("我的店铺").exists()) {
+            sleep(intervalTimeMillisecond);
+            className("android.widget.ImageView").clickable(true).depth(9).findOne().click(); // 返回按钮
+            sleep(intervalTimeMillisecond);
+        }
+    } else {
+        toastLog("未发现任务");
     }
 
-    //死循环
-    while (true) ;
-} else {
-    toastLog("请关闭当前App，然后再启动脚本！");
+    if (className("android.view.View").descContains("我的店铺").exists()) {
+        sleep(intervalTimeMillisecond);
+        className("android.widget.ImageView").clickable(true).depth(9).findOne().click(); // 返回按钮
+        sleep(intervalTimeMillisecond);
+    }
+
+    sleep(waitTimeMillisecond);
 }
 
 sleep(5000);
